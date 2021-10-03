d5ecad27d08bbe0473899f84961daa7243583498
Nack Itt <nackda@protonmail.com>
2021-10-03 17:56:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from urllib.parse import quote
-import hashlib
+import hashlib, requests
 
-class Pilgrim:
-    getTrack = lambda trackId : "https://api-music.sanook.com/openjoox2/v1/track/{}?country=th&lang=en&lyric=1&fs=1&secret={}".format(trackId, Pilgrim.getSecret(trackId))
-    todayAlbum = "https://jooxweb-api.tencent.co.th/openapi/footer/all?region=th-th"
+class JOOX:
+    """
+    bruh who tf wakes up and thinks that it’s a good idea to put 'secret' in plain text.
+    """
+    
+    def request(_): return requests.get(_).json()
+    def getSecret(trackId): return hashlib.md5((""+"SaNo0k@t3Nc3nT"+"country=th&lang=en&lyric=1&fs=1&id={}".format(quote(trackId))).encode("UTF-8")).hexdigest()
 
-    def getSecret(trackId):
-        secretKey = "SaNo0k@t3Nc3nT"
-        quoteBuilder = "country=th&lang=en&lyric=1&fs=1&id={}".format(quote(trackId)) # too lazy to use lambda, map, keys. lol
-        return hashlib.md5((""+secretKey+quoteBuilder).encode("UTF-8")).hexdigest()
-
+    def getTrack(trackId): return JOOX.request("https://api-music.sanook.com/openjoox2/v1/track/{}?country=th&lang=en&lyric=1&fs=1&secret={}".format(trackId,JOOX.getSecret(trackId)))
+    def getTrending(): return JOOX.request("https://jooxweb-api.tencent.co.th/openapi/footer/all?region=th-th")
+    def getQuery(keyword): return JOOX.request("https://api-jooxtt.sanook.com/openjoox/v3/search?country=th&lang=th&keyword={}".format(keyword))
+    def getAlbum(albumId): return JOOX.request("https://api-jooxtt.sanook.com/page/albumDetail?id={}&lang=th&country=th&device=desktop".format(albumId))
+    
 if __name__ == "__main__":
-    print(Pilgrim.getTrack("FX05hvgrJ9N3VOX6n3ZFlA==")) # https://api-music.sanook.com/openjoox2/v1/track/FX05hvgrJ9N3VOX6n3ZFlA==?country=th&lang=en&lyric=1&fs=1&secret=33da9f3c81f56ec3df689badd402d85b
+    print(
+        JOOX.getTrack("FX05hvgrJ9N3VOX6n3ZFlA=="), # track-info
+        JOOX.getTrending(), # recommended albums
+        JOOX.getQuery("tuayp") # search
+        JOOX.getAlbum("OcAQEWktgfUNGWXJep1fkw==") # album-info
+    )
